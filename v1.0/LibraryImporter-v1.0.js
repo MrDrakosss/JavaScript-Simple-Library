e83a4b9a5b5b387205d321318c8a3c7a0ef7aa59
MrDrakosss <70947713+MrDrakosss@users.noreply.github.com>
2023-10-02 23:18:59 +0200

    Update LibraryImporter-v1.0.js

--- v1.0/LibraryImporter-v1.0.js
+++ v1.0/LibraryImporter-v1.0.js
@@ -10,52 +10,33 @@ class LibraryImporter {
             console.log('jQuery already loaded');
             this.jquery = !this.jquery;
         } else {
-            console.info('jQuery not found');
-            console.info('jQuery loading..')
+            console.info('[LibraryImporter] jQuery not found');
+            console.info('[LibraryImporter] jQuery loading..')
             var script = document.createElement("SCRIPT");
             script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
             script.type = 'text/javascript';
             document.getElementsByTagName("head")[0].appendChild(script);
-            console.info('jQuery successfully imported');
-
-            
-            script = document.createElement("SCRIPT");
-            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/jQuery/array.js';
-            script.type = 'text/javascript';
-            document.getElementsByTagName("head")[0].appendChild(script);
-
-            script = document.createElement("SCRIPT");
-            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/jQuery/math.js';
-            script.type = 'text/javascript';
-            document.getElementsByTagName("head")[0].appendChild(script);
-
-            script = document.createElement("SCRIPT");
-            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/jQuery/string.js';
-            script.type = 'text/javascript';
-            document.getElementsByTagName("head")[0].appendChild(script);
+            console.info('[LibraryImporter] jQuery successfully imported');
         }
 
-        if (!window.jQuery) {
-            console.error('Failed to install jQuery ');
-            console.info('SimpleLibrary version setup: Normal version');
-
+        if (window.jQuery) {
             var script = document.createElement("SCRIPT");
-            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/normal/array.js';
+            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/array.js';
             script.type = 'text/javascript';
             document.getElementsByTagName("head")[0].appendChild(script);
 
             script = document.createElement("SCRIPT");
-            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/normal/math.js';
+            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/math.js';
             script.type = 'text/javascript';
             document.getElementsByTagName("head")[0].appendChild(script);
 
             script = document.createElement("SCRIPT");
-            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/normal/string.js';
+            script.src = 'https://raw.githubusercontent.com/MrDrakosss/JavaScript-Simple-Library/main/v1.0/string.js';
             script.type = 'text/javascript';
             document.getElementsByTagName("head")[0].appendChild(script);
-
         } else {
-            console.error('SimpleLibrary version setup: jQuery version');
+            console.log('[LibraryImporter] jQuery not loaded!');
+            return;
         }
     }
 }
